71d567f1684a2a5c56ac58d03440e116366e2c00
Rei Sato <102862471+OSAlly-attr@users.noreply.github.com>
2023-05-14 12:24:40 +0900

    Update func.py

--- func.py
+++ func.py
@@ -27,7 +27,7 @@ def chrome():
 
 # LINEに送信する関数
 def send_line(text):
-    TOKEN =  'Ghbzj30VWqKNzYTk1dZnh3NjkjRUntNJdUxCQNfEkOx'
+    TOKEN =  ''
     api_url = 'https://notify-api.line.me/api/notify'
     send_text = text
     TOKEN_dic = {'Authorization': 'Bearer ' + TOKEN}
